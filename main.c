cee435f7ac06643831dda13e9a91e4b2321d5f28
iristaub <59415717+iristaub@users.noreply.github.com>
2020-05-04 19:26:24 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 /*
  * main.c
  *
- *  Created on: 2 May 2020
- *      Author: Yaniv Blum
+ *  Created on: 2 במאי 2020
+ *      Author: irist
  */
+
 #include "spmat.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,6 +12,7 @@
 #include <time.h>
 #include "SPBufferset.h"
 #include <math.h>
+#include <string.h>
 
 static const double EPSILON = 0.00001;
 
@@ -22,29 +24,30 @@ void initialize_random_vector(double *currvector, int n){
 	}
 }
 
-void read_sparse_matrix(spmat matrix, int n, char flag, FILE input){
-	if(flag == 'l'){
-		read_in_list_format(matrix, n, input);
-	}
-	else{
-		read_in_array_format(matrix, n, input);
-	}
-}
 
-void read_in_list_format(spmat matrix, int n, FILE input){
+
+spmat* read_in_list_format(int n, FILE* input){
 	int i, d;
 	double *temp_row;
+	spmat* matrix = (spmat*)malloc(sizeof(spmat));
+	printf("start read_in_list_format \n");
+	printf("n = %d \n", n);
 	matrix = spmat_allocate_list(n);
 	temp_row = (double*)calloc(n, sizeof(double));
 	for(i = 0; i < n; i++){
-		d = fread(&temp_row, sizeof(double), n, input);
+		/*printf("start iteration i = %d \n", i);*/
+		d = fread(temp_row, sizeof(double), n, input);
 		assert(d == n);
-		add_row(matrix, temp_row, i);
+		/*printf("done reading \n");*/
+		matrix->add_row(matrix, temp_row, i);
+		/*printf("done iteration i = %d", i);*/
 	}
 	free(temp_row);
+	return matrix;
 }
 
-void read_in_array_format(spmat matrix, int n, FILE input){
+spmat* read_in_array_format(int n, FILE* input){
+	spmat* matrix = (spmat*)malloc(sizeof(spmat));
 	int nnz, d, i;
 	double num;
 	double *temp_row;
@@ -63,12 +66,25 @@ void read_in_array_format(spmat matrix, int n, FILE input){
 	for(i = 0; i < n; i++){
 		d = fread(&temp_row, sizeof(double), n, input);
 		assert(d == n);
-		add_row(matrix, temp_row, i);
+		matrix->add_row(matrix, temp_row, i);
 	}
 	free(temp_row);
+	return matrix;
 }
 
-double clacnorm(double *nextvector, int n){
+spmat* read_sparse_matrix(int n, char flag, FILE* input){
+	spmat* matrix = (spmat*)malloc(sizeof(spmat));
+	printf("reading matrix \n");
+	if(flag == 'l'){
+		matrix = read_in_list_format(n, input);
+	}
+	else{
+		matrix = read_in_array_format(n, input);
+	}
+	return matrix;
+}
+
+double calcnorm(double *nextvector, int n){
 	int i;
 	double sum;
 	sum = 0.0;
@@ -79,9 +95,11 @@ double clacnorm(double *nextvector, int n){
 }
 
 
-void poweriteration(spmat matrix, double *currvector, double *nextvector, int n){
-	int norm, i;
-	mult(matrix, currvector, nextvector);
+void poweriteration(spmat *matrix, double *currvector, double *nextvector, int n){
+	int i;
+	double norm;
+	printf("start poweriteration \n");
+	matrix->mult(matrix, currvector, nextvector);
 	norm = calcnorm(nextvector, n);
 	for(i = 0; i < n; i++){
 		nextvector[i] = (double)nextvector[i]/norm;
@@ -98,8 +116,9 @@ int check(double *currvector, double *nextvector, int n){
 	return 1;
 }
 
-void calc_eigen(spmat matrix, double *currvector, double *nextvector, int n){
+void calc_eigen(spmat *matrix, double *currvector, double *nextvector, int n){
 	int i;
+	printf("start calc_eigen \n");
 	poweriteration(matrix, currvector, nextvector, n);
 	while(check(currvector, nextvector, n) != 1){
 		for(i = 0; i < n; i++){
@@ -109,16 +128,16 @@ void calc_eigen(spmat matrix, double *currvector, double *nextvector, int n){
 	}
 }
 
-void write_eigen_to_outf(double nextvector, int n, FILE output){
+void write_eigen_to_outf(double *nextvector, int n, FILE* output){
 	int d;
 	d = fwrite(&n, sizeof(int), 1, output);
 	assert(d == 1);
-	d = fwrite(nextvector, sizeof(double), n, output);
+	d = fwrite(&nextvector, sizeof(double), n, output);
 	assert(d == n);
 }
 
 int main(int argc, char* argv[]){
-	FILE* input, output, initvector;
+	FILE *input, *output, *initvector;
 	double *currvector, *nextvector;
 	spmat *matrix;
 	int n, d;
@@ -127,45 +146,49 @@ int main(int argc, char* argv[]){
 	double time1;
 	SP_BUFF_SET();
 	start = clock();
+	printf("%s", argv[1]);
+	printf("%s", argv[2]);
+	printf("%s", argv[3]);
+	printf("%s", argv[4]);
 	input = fopen(argv[1], "r");/*get the input matrix*/
 	d = fread(&n, sizeof(int), 1, input);
 	assert(d == 1);
 	currvector = (double*)calloc(n, sizeof(double));
 	nextvector = (double*)calloc(n, sizeof(double));
 	if(argc == 4){/*the initial vector is not given*/
-		initialize_random_vector(currvector);
-		output = fopen(argv[2], "w");
-		if(argv[3] == "-list"){
-			flag = 'l';
-		}
-		else{
-			flag = 'a';
-		}
+		initialize_random_vector(currvector, n);
 	}
 	else{
 		assert(argc == 5); /*the initial vector is given*/
 		initvector = fopen(argv[2], "r");
-		d = fread(&currvector, sizeof(double), n, initvector);
+		printf("init vector file opend \n");
+		/*d = fread(&n, sizeof(int), 1, initvector);*/
+		/*assert(d == 1);*/
+		d = fread(currvector, sizeof(double), n, initvector);
 		assert(d == n);
-		output = fopen(argv[3], "w");
-		if(argv[4] == "-list"){
-			flag = 'l';
-		}
-		else{
-			flag = 'a';
-		}
+		printf("initvector file opend \n");
 	}
-	read_sparse_matrix(matrix, n, flag, input);
+	if(strcmp(argv[argc - 1],"-list") == 0){
+		flag = 'l';
+	}
+	else{
+		flag = 'a';
+	}
+	printf("\n");
+	printf("done initialising \n");
+	matrix = read_sparse_matrix(n, flag, input);
+	printf("finish reading \n");
 	calc_eigen(matrix, currvector, nextvector, n);
+	output = fopen(argv[argc - 2], "w");
 	write_eigen_to_outf(nextvector, n, output);
 	end = clock();
-	/*time1 = ((double) (end - start) / CLOCKS_PER_SEC);*/
+	time1 = ((double) (end - start) / CLOCKS_PER_SEC);
+	printf("time to run: %f.5",time1);
 	fclose(input);
 	fclose(output);
 	free(currvector);
 	free(nextvector);
-	free(matrix);
+	matrix->free(matrix);
 	return 0;
 
 }
-
